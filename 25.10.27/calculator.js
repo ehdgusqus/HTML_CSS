da147295815fd3c52eb694fcd65956e4b734fcdc
ByunDongHyun <ehdgusqus55@gamil.com>
2025-10-27 15:06:03 +0900

    update calculator

---
+++ 25.10.27/calculator.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {   
+    // 사칙 연산 계산기 
+    // id 불러오기
+
+
+    // event
+    calcForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // 페이지 새로고침 방지
+
+        // 입력 값 가져오기
+
+
+
+        // 연산 수행 (switch문으로 구현, 나눗셈에서 분모에 0이 들어가는 경우 예외 처리)
+        
+        // 결과 표시
+        
+    });
+
+    // ============================================================================
+    // 큰 수 찾기 
+    // id 불러오기
+
+    // event
+    maxForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // 페이지 새로고침 방지
+
+        // 입력 값 가져오기
+       
+        
+        // 유효성 검사
+
+
+        // 가장 큰 수 찾기
+
+        
+        // 결과 표시
+
+    });
+
+});
